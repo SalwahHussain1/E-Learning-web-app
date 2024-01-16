40a9d4e7850a7803e1fa3ba0fdd8a32ef92044ad
SalwahHussain1 <salwahhussain2@gmail.com>
2024-01-16 10:41:07 -0800

    first app

---
+++ src/components/gallery.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          <p>
+          Immerse yourself in a visual feast with our gallery view on the e-commerce website – a captivating showcase of products that brings your shopping experience to life. Explore each item up close, making informed decisions with a stunning, seamless, and interactive display."
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
